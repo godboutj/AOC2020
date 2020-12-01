1f86886c62b9070be12f829c0264add465dba00c
godboutj <godboutj@amotus.ca>
2020-12-01 11:04:35 -0500

    Single load and fix using the same number with itself, not sure this is allowed

--- 1/Main.py
+++ 1/Main.py
@@ -2,35 +2,33 @@ import re
 import os
 import sys
 
-def RunTest1():
-    print("Test 1")
+def LoadData():
     input_number = []
     with open('input.txt') as inputFile:
         line = inputFile.readline()
         while len(line) > 0:
             input_number.append(int(line))
             line = inputFile.readline()
+    return input_number
+
+def RunTest1(input_number):
+    print("Test 1")
     nb_elem = len(input_number)
     for i in range(0, nb_elem - 1):
-        for j in range(i, nb_elem):
+        for j in range(i + 1, nb_elem):
             if (input_number[i] + input_number[j]) == 2020:
                 print("Solution: {0} {1} {2}".format(input_number[i], input_number[j], input_number[i] * input_number[j]))
 
-def RunTest2():
+def RunTest2(input_number):
     print("Test 2")
-    input_number = []
-    with open('input.txt') as inputFile:
-        line = inputFile.readline()
-        while len(line) > 0:
-            input_number.append(int(line))
-            line = inputFile.readline()
     nb_elem = len(input_number)
-    for i in range(0, nb_elem - 1):
-        for j in range(i, nb_elem):
-            for k in range(j, nb_elem):
+    for i in range(0, nb_elem - 2):
+        for j in range(i + 1, nb_elem):
+            for k in range(j + 1, nb_elem):
                 if (input_number[i] + input_number[j] + input_number[k]) == 2020:
                     print("Solution: {0} {1} {2} {3}".format(input_number[i], input_number[j], input_number[k], input_number[i] * input_number[j] * input_number[k]))
 
 if __name__ == "__main__":
-    RunTest1()
-    RunTest2()
+    data = LoadData()
+    RunTest1(data)
+    RunTest2(data)
